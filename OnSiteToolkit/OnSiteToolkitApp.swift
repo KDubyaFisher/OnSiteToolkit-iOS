887373d94f8930c48b584ce9d5dea9f340fc51ec
Kolton Fisher <fisher.kolton@gmail.com>
2026-04-27 22:51:50 -0500

    Initial Commit

---
+++ OnSiteToolkit/OnSiteToolkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnSiteToolkitApp.swift
+//  OnSiteToolkit
+//
+//  Created by Kolton Fisher on 4/27/26.
+//
+
+import SwiftUI
+
+@main
+struct OnSiteToolkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
